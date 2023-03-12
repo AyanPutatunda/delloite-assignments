c794c4354a501fdb22f1dd37f37b9ee16a5bbbee
AyanPutatunda <39451863+AyanPutatunda@users.noreply.github.com>
2023-03-12 19:51:18 +0000

    final code commit

--- src/etl.py
+++ src/etl.py
@@ -10,7 +10,6 @@ def get_files(path):
 
     Parameters:
         path(str):The folder path.
-
     Returns:
         res(str):The file name in the folder.
     """
@@ -24,12 +23,11 @@ def ingest_excel_file_pandas(excel_file_name):
 
     Parameters:
         excel_file_name(str):The excel file name.
-
     Returns:
         df(pandas dataframe):The pandas dataframe with the data loaded from excel file. 
     """
     df = pd.read_excel(r'data/'+excel_file_name, sheet_name='DATA')
-    print('Data from  excel file has been loaded to pandas dataframe')
+    print('Data from  excel file has been loaded to pandas dataframe.....')
     return df
 
 def transform_data(data_to_transform, file_name_for_df):
@@ -39,29 +37,21 @@ def transform_data(data_to_transform, file_name_for_df):
     Parameters:
         file_name_for_df(str):The excel file name.
         data_to_transform(pandas dataframe):The pandas dataframe containing the data for transformation
-
     Returns:
         data_to_transform(pandas dataframe):The pandas dataframe post transformation.
-
         The transformations are : 
             1. added an index column which is unique and not null.
             2. added file_name column which has the source file name
             3. added updated_timestamp column which has the ingestion timestamp.
             4. converted the data in columns first_name,last_nam and gender column to lower case.
     """
-    pass
-    print('Data before transformation')
-    print(data_to_transform)
     data_to_transform['first_name'] = data_to_transform['first_name'].str.lower()
     data_to_transform['last_name'] = data_to_transform['last_name'].str.lower()
     data_to_transform['gender'] = data_to_transform['gender'].str.lower()
-    # df['Courses'] = df['Courses'].str.lower()
     data_to_transform['file_name'] = file_name_for_df
     data_to_transform['updated_timestamp'] = datetime.now()
-    print('Data after transformation')
-    print(data_to_transform)
+    print('Data is pandas dataframe has been transformed.....')
     return data_to_transform
-
 
 def load_data_to_postgres(df_to_ingest,connection_string):
     """
@@ -70,17 +60,14 @@ def load_data_to_postgres(df_to_ingest,connection_string):
     Parameters:
         file_name_for_df(str):The excel file name.
         connection_string(str):The postgresql db connection string.
-
     Returns:
         data_to_transform(pandas dataframe):The pandas dataframe post transformation.
-
     """
     # engine = create_engine('postgresql://postgres:password@database-1.cgvdfgv8p3kj.us-west-1.rds.amazonaws.com:5432/postgres')
     engine = create_engine(connection_string)
     df_to_ingest.to_sql('ayan_test', engine, if_exists='replace')
-    print('Data ingested in postgres successfully')
+    print('Data ingested in postgres successfully.....')
     
-
 if __name__ == "__main__":
     file_names = get_files('data/')
     connection_string = 'postgresql://postgres:password@database-1.cgvdfgv8p3kj.us-west-1.rds.amazonaws.com:5432/postgres'
